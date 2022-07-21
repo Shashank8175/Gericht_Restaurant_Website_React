1bf1d53f053343aac1a8f349080e9d968e324da6
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-21 22:46:32 +0530

    Create SpecialMenu Section.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { images, data } from '../../constants'
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='Menu'>
+    <div className="app__specialMenu-title">
+      <SubHeading title='Menu that fits you palatte'/>
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
   </div>
 );
 
